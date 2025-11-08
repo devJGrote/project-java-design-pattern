15e4e1c681b458b3edaeb67b89f682aaff7f125d
Jens <mail@j-grote.de>
2025-11-08 11:22:04 +0100

    step2

---
+++ src/main/java/de/inosofttech/example/viergewinnt/Spieler.java
@@ -0,0 +1,14 @@
+package de.inosofttech.example.viergewinnt;
+
+public class Spieler {
+    private char spielerZeichen;
+
+    public Spieler(char spielerZeichen) {
+        this.spielerZeichen = spielerZeichen;
+    }
+
+    public char getZeichen() {
+        return spielerZeichen;
+    }
+
+}
